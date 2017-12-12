b961fc2b154f6a0e6dac1ab92472bb8f2e003911
Adam Mescher <admescher@gmail.com>
2017-12-12 14:30:19 -0700

    Create DB and seed data

---
+++ db/migrations/20171212113453_initial.js
@@ -0,0 +1,31 @@
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('teams', table => {
+      table.increments('id').primary();
+      table.string('city');
+      table.string('name');
+      table.timestamps(true, true);
+    }),
+    knex.schema.createTable('players', table => {
+      table.increments('id').primary();
+      table.integer('team_id').unsigned();
+      table.foreign('team_id').references('teams.id');
+      table.integer('number').unsigned();
+      table.string('name');
+      table.string('position');
+      table.integer('age').unsigned();
+      table.string('height');
+      table.integer('weight').unsigned();
+      table.string('experience');
+      table.string('college');
+      table.timestamps(true, true);
+    })
+  ]);
+};
+
+exports.down = function(knex, Promise) {
+  return Promse.all([
+    knex.schema.dropTable('players'),
+    knex.schema.dropTable('teams')
+  ]);
+};
---
+++ db/seeds/dev/teams.js
@@ -0,0 +1,63 @@
+const teams = require('../../../scrape/nfl-players');
+const teamAbbreviations = Object.keys(teams); 
+
+const createTeam = (knex, team) => {
+  return knex('teams').insert({
+    city: team.city,
+    name: team.name
+  }, 'id')
+    .then(teamID => {
+      let playerPromises = [];
+
+      team.players.forEach( (player, index) => {
+        if ( isNaN(player.number)) {
+          console.log(player);
+        }
+
+        if ( isNaN(player.age)){
+          console.log(player);
+        }
+
+        if (isNaN(player.weight)) {
+          console.log(player);
+        }
+
+
+        playerPromises.push(
+          createPlayer(knex, {
+            team_id: teamID[0],
+            number: parseInt(player.number, 10),
+            name: player.name,
+            position: player.position,
+            age: parseInt(player.age, 10),
+            height: player.height,
+            weight: parseInt(player.weight, 10),
+            experience: player.experience,
+            college: player.college 
+          })
+        )
+      });
+
+      return Promise.all(playerPromises);
+    });
+};
+
+const createPlayer = (knex, player) => {
+  return knex('players').insert(player);
+};
+
+exports.seed = (knex, Promise) => {
+  return knex('players').del()
+    .then(() => knex('teams').del() )
+      .then(() => {
+        console.log('TEAMS TABLE DELETED');
+        let teamPromises = [];
+
+        teamAbbreviations.forEach( abbreviation => {
+          teamPromises.push(createTeam(knex, teams[abbreviation]));
+        })
+        console.log('SUCCESSFUL SEED');
+        return Promise.all(teamPromises);
+      })
+    .catch(error => console.log(`Error seeding data: ${error}`));
+};
