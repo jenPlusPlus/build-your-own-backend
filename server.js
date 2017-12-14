35a788ca545a098f2ed63909832b3abdda07eda3
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-12-14 13:54:54 -0700

    Lint server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+/* eslint no-restricted-syntax: 0 */
+/* eslint no-console: 0 */
+
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken');
@@ -19,30 +24,30 @@ const database = require('knex')(configuration);
 const checkAuth = (request, response, next) => {
   const token = request.body.token || request.query.token || request.headers['x-access-token'];
 
-  if(!token) {
-    return response.status(403).json({error: 'You must be authorized to access this endpoint.'});
-  } else {
-    const verified = jwt.verify(token, app.get('secretKey'))
-
-    if (verified.admin) {
-      next();
-    } else {
-      return response.status(403).json({error: 'You must be authorized to access this endpoint.'})
-    }
+  if (!token) {
+    return response.status(403).json({ error: 'You must be authorized to access this endpoint.' });
   }
-}
+  const verified = jwt.verify(token, app.get('secretKey'));
 
-app.use(express.static(__dirname + '/public'));
+  if (verified.admin) {
+    next();
+  } else {
+    return response.status(403).json({ error: 'You must be authorized to access this endpoint.' });
+  }
+  return null;
+};
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.post('/api/v1/authenticate', (request, response) => {
-  const { email, appName } = request.body;
+  const { email } = request.body;
 
   // rendundant check for required parameters
-  for (let requiredParameter of ['email', 'appName']) {
+  for (const requiredParameter of ['email', 'appName']) {
     if (!request.body[requiredParameter]) {
       return response.send(422).json({ error: `You are missing the '${requiredParameter}' property` });
-    };
-  };
+    }
+  }
 
   const admin = email.endsWith('@turing.io');
   const token = jwt.sign({ admin }, app.get('secretKey'));
@@ -59,8 +64,8 @@ app.get('/api/v1/teams', (request, response) => {
       .catch(error => response.status(500).json({ error }));
   } else {
     database('teams').where(queryParameter, queryParameterValue).select()
-      .then(team => {
-        if(!team.length){
+      .then((team) => {
+        if (!team.length) {
           return response.status(404).json({ error: `Could not find any team associated with '${queryParameter}' of '${queryParameterValue}'` });
         }
         return response.status(200).json({ team: team[0] });
@@ -71,8 +76,8 @@ app.get('/api/v1/teams', (request, response) => {
 
 app.get('/api/v1/teams/:id', (request, response) => {
   database('teams').where('id', request.params.id).select()
-    .then(team => {
-      if(team.length){
+    .then((team) => {
+      if (team.length) {
         return response.status(200).json({ team: team[0] });
       }
       return response.status(404).json({ error: `Could not find any team associated with id ${request.params.id}` });
@@ -81,18 +86,20 @@ app.get('/api/v1/teams/:id', (request, response) => {
 });
 
 app.post('/api/v1/teams', checkAuth, (request, response) => {
-  let { name, city } = request.body;
-  let team = { name, city };
+  const { name, city } = request.body;
+  const team = { name, city };
 
-  for (let requiredParameter of ['city', 'name']) {
+  for (const requiredParameter of ['city', 'name']) {
     if (!team[requiredParameter]) {
       return response.status(422).json({ error: `You are missing the '${requiredParameter}' property` });
-    };
-  };
+    }
+  }
 
   database('teams').insert(team, 'id')
-    .then(team => response.status(201).json({ id: team[0] }))
-    .catch(error => response.status(500).json({ error }))
+    .then(insertedTeam => response.status(201).json({ id: insertedTeam[0] }))
+    .catch(error => response.status(500).json({ error }));
+
+  return null;
 });
 
 app.patch('/api/v1/teams/:id', checkAuth, (request, response) => {
@@ -101,35 +108,35 @@ app.patch('/api/v1/teams/:id', checkAuth, (request, response) => {
   const body = { city, name };
 
   database('teams').where('id', teamID).update(body, '*')
-    .then(team => {
+    .then((team) => {
       if (!team.length) {
         return response.status(404).json({ error: `Could not find any team associated with team_id ${request.params.id}` });
       }
       return response.status(202).json({ team: team[0] });
     })
     .catch(error => response.status(500).json({ error }));
-})
+});
 
 app.delete('/api/v1/teams/:id', checkAuth, (request, response) => {
   const teamID = request.params.id;
 
   database('teams').where('id', teamID).del()
     .then(() => response.status(204).json({ teamID }))
-    .catch(error => response.status(404).json({error: `Could not find team with id '${teamID}'`}));
+    .catch(() => response.status(404).json({ error: `Could not find team with id '${teamID}'` }));
 });
 
 app.get('/api/v1/players', (request, response) => {
   const queryParameter = Object.keys(request.query)[0];
   const queryParameterValue = request.query[queryParameter];
 
-  if(!queryParameter){
+  if (!queryParameter) {
     database('players').select()
       .then(players => response.status(200).json({ players }))
       .catch(error => response.status(500).json({ error }));
   } else {
     database('players').where(queryParameter, queryParameterValue).select()
-      .then(player => {
-        if(!player.length){
+      .then((player) => {
+        if (!player.length) {
           return response.status(404).json({ error: `Could not find any player associated with '${queryParameter}' of '${queryParameterValue}'` });
         }
         return response.status(200).json({ player: player[0] });
@@ -140,8 +147,8 @@ app.get('/api/v1/players', (request, response) => {
 
 app.get('/api/v1/players/:id', (request, response) => {
   database('players').where('id', request.params.id).select()
-    .then(player => {
-      if(player.length){
+    .then((player) => {
+      if (player.length) {
         return response.status(200).json({ player: player[0] });
       }
       return response.status(404).json({ error: `Could not find any player associated with id ${request.params.id}` });
@@ -149,21 +156,21 @@ app.get('/api/v1/players/:id', (request, response) => {
     .catch(error => response.status(500).json({ error }));
 });
 
-app.get('/api/v1/teams/:id/players', (request,response) => {
+app.get('/api/v1/teams/:id/players', (request, response) => {
   database('players').where('team_id', request.params.id).select()
-    .then(players => {
-      if(players.length){
+    .then((players) => {
+      if (players.length) {
         return response.status(200).json({ players });
       }
-      return response.status(404).json({ error: `Could not find any players with a team_id of ${request.params.id}`});
+      return response.status(404).json({ error: `Could not find any players with a team_id of ${request.params.id}` });
     })
     .catch(error => response.status(500).json({ error }));
 });
 
 app.get('/api/v1/teams/:teamID/players/:playerID', (request, response) => {
   database('players').where('team_id', request.params.teamID).where('id', request.params.playerID).select()
-    .then(player => {
-      if(player.length){
+    .then((player) => {
+      if (player.length) {
         return response.status(200).json({ player });
       }
       return response.status(404).json({ error: `Could not find any player associated with team_id ${request.params.teamID} and id ${request.params.playerID}` });
@@ -172,59 +179,68 @@ app.get('/api/v1/teams/:teamID/players/:playerID', (request, response) => {
 });
 
 app.post('/api/v1/teams/:id/players', checkAuth, (request, response) => {
-  const { number, name, position, age, height, weight, experience, college } = request.body;
-  let player = { number, name, position, age, height, weight, experience, college };
+  const {
+    number, name, position, age, height, weight, experience, college,
+  } = request.body;
+  let player = {
+    number, name, position, age, height, weight, experience, college,
+  };
   const { id } = request.params;
 
-  for (let requiredParameter of ['number', 'name', 'position', 'age', 'height', 'weight', 'experience', 'college']) {
+  for (const requiredParameter of ['number', 'name', 'position', 'age', 'height', 'weight', 'experience', 'college']) {
     if (!player[requiredParameter]) {
       return response.status(422).json({ error: `You are missing the '${requiredParameter}' property` });
-    };
-  };
+    }
+  }
 
-  if(player.number < 0){
-   return response.status(422).json({ error: `number must be a positive number` });
+  if (player.number < 0) {
+    return response.status(422).json({ error: 'number must be a positive number' });
   } else if (player.age < 0) {
-    return response.status(422).json({ error: `age must be a positive number` });
+    return response.status(422).json({ error: 'age must be a positive number' });
   } else if (player.weight < 0) {
-    return response.status(422).json({ error: `weight must be a positive number` });
+    return response.status(422).json({ error: 'weight must be a positive number' });
   }
 
   player = Object.assign({}, player, { team_id: id });
 
   database('players').insert(player, 'id')
-    .then(player => response.status(201).json({ id: player[0] }))
-    .catch(error => response.status(500).json({ error }))
+    .then(insertedPlayer => response.status(201).json({ id: insertedPlayer[0] }))
+    .catch(error => response.status(500).json({ error }));
+
+  return null;
 });
 
 app.patch('/api/v1/teams/:teamID/players/:playerID', checkAuth, (request, response) => {
-  const { teamID, playerID } = request.params;
-  const { number, name, position, age, height, weight, experience, college } = request.body;
-  const body = { number, name, position, age, height, weight, experience, college };
+  const { playerID } = request.params;
+  const {
+    number, name, position, age, height, weight, experience, college,
+  } = request.body;
+  const body = {
+    number, name, position, age, height, weight, experience, college,
+  };
 
   database('players').where('id', playerID).update(body, '*')
-    .then(player => {
+    .then((player) => {
       if (!player.length) {
         return response.status(404).json({ error: `Could not find any player associated with team_id ${request.params.teamID} and id ${request.params.playerID}` });
       }
       return response.status(202).json({ player: player[0] });
     })
     .catch(error => response.status(500).json({ error }));
-})
+});
 
 app.delete('/api/v1/teams/:teamID/players/:playerID', checkAuth, (request, response) => {
-  const { teamID, playerID } = request.params;
+  const { playerID } = request.params;
 
   database('players').where('id', playerID).del()
     .then(() => response.status(204).json({ playerID }))
-    .catch(error => response.status(404).json({error: `Could not find player with id '${playerID}'`}));
+    .catch(() => response.status(404).json({ error: `Could not find player with id '${playerID}'` }));
 });
 
-app.use(function (request, response, next) {
-  return response.status(404).send("404: Sorry can't find that!");
-});
+app.use((request, response) => response.status(404).send("404: Sorry can't find that!"));
 
-app.use(function (error, request, response, next) {
+
+app.use((error, request, response) => {
   console.error(error.stack);
   return response.status(500).send('Something broke!');
 });
