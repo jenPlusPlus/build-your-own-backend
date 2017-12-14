a1097db290fb807914881ab766d0b3e7da9006d8
Jennifer Woodson <jenniferpwoodson@gmail.com>
2017-12-14 13:34:25 -0700

    Install linter and correct knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,27 +2,27 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      filename: 'postgres://localhost/byob'
+      filename: 'postgres://localhost/byob',
     },
     migrations: {
-      directory: './db/migrations'
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds/dev',
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
   },
   test: {
     client: 'pg',
     connection: {
-      filename: process.env.DATABASE_URL || 'postgres://localhost/byob_test'
+      filename: process.env.DATABASE_URL || 'postgres://localhost/byob_test',
     },
     migrations: {
-      directory: './db/migrations'
+      directory: './db/migrations',
     },
     seeds: {
-      directory: './db/seeds/test'
+      directory: './db/seeds/test',
     },
-    useNullAsDefault: true
-  }
+    useNullAsDefault: true,
+  },
 };
